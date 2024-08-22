cd9af351531c6ee00d0684e8805364263d1333c0
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-22 16:19:20 -0400

    create home page

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 # load_dotenv()
 # ENV = os.getenv("ENV")
+
+app = Flask("Weather Data Website")
+
+
+# When the user visits the site the index.htlm file will be shown
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+app.run(debug=True)
+
 
